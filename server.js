cd9d9c1507dc070e127c98a77804a8f3a0a99da8
Sara2829 <2021bec102@sggs.ac.in>
2024-09-16 01:02:43 +0530

    without socket

--- server.js
+++ server.js
@@ -50,36 +50,3 @@ app.listen(PORT, (err) => {
     console.log(`Server started on port ${PORT}`);
   }
 });
-
-// // Socket.IO
-// const io = new Server(ser, {
-//   cors: {
-//     origin: "*", // Allow all origins
-//   },
-// });
-
-// io.on("connection", (socket) => {
-//   console.log("A user connected:", socket.id);
-
-//   socket.on("setup", (userId) => {
-//     socket.join(userId);
-//     socket.emit("connected", userId);
-//     console.log("User connected to socket:", userId);
-//   });
-
-//   socket.on("joinroom", (room) => {
-//     socket.join(room);
-//     console.log("User joined room:", room);
-//   });
-
-//   // Listening for messages
-//   socket.on("message", (data) => {
-//     console.log("Message received:", data);
-//     io.emit("message", data); // Broadcast to all clients
-//   });
-
-//   // Handle disconnection
-//   socket.on("disconnect", () => {
-//     console.log("A user disconnected:", socket.id);
-//   });
-// });
